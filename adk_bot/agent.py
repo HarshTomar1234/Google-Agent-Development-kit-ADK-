d5d7e9bb209b1167651847adb83ce40045764570
Harsh Tomar <123729493+HarshTomar1234@users.noreply.github.com>
2025-05-06 20:56:36 +0530

    Add files via upload

---
+++ adk_bot/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+
+from adk_bot.prompt import ROOT_AGENT_INSTRUCTION
+
+from adk_bot.tools import count_characters
+
+root_agent = Agent(
+    name="adk_bot",
+    model="gemini-2.0-flash",
+    description="A bot that shortens messages while maintaining their core meaning",
+    instruction=ROOT_AGENT_INSTRUCTION,
+    tools=[count_characters],
+)
